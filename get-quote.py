3bd14b066661a7a863c709220a5619f4b7c3c37c
Fabrizio Borrello <borrellofabrizio@gmail.com>
2020-01-06 14:39:49 +0100

    add random module for chose quote

--- get-quote.py
+++ get-quote.py
@@ -1,11 +1,14 @@
+import random
+
 def primary():
   print("Keep it logically awesome.")
 
-  #f = open("quotes.txt")
-  #quotes = f.readlines()
-  #f.close()
-
-  #print(quotes)
+  f = open('C:/Users/borre/github/python-random-quote/quotes.txt')
+  quotes = f.readlines()
+  f.close()
+  last = len(quotes) - 1
+  rnd_quote_index = random.randint(0, last)
+  print(quotes[rnd_quote_index])
 
 if __name__== "__main__":
   primary()
